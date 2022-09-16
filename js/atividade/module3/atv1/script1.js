14205c76bc017c51de62535122b6a5f653f1f70b
Jean Silva <je.ang.10@hotmail.com>
2022-09-16 00:11:46 -0300

    atv

---
+++ js/atividade/module3/atv1/script1.js
@@ -0,0 +1,9 @@
+let nota1 = 10
+let nota2 = 6
+let nota3 = 5
+
+let soma = (nota1+nota2+nota3) / 3
+
+let media = soma >= 7 ? "Aprovado" : "Reprovado"
+
+console.log("Sua média foi: "+ soma + " - Você está: " +media)
---
+++ js/atividade/module3/atv1/script2.js
@@ -0,0 +1,10 @@
+let nota1 = 6
+let nota2 = 6
+
+let notaSomada = nota1+nota2
+
+let notaAprovacao = 21
+
+let notaNecessaria = notaAprovacao - notaSomada
+
+console.log("Você precisa tirar na próxima prova: " + notaNecessaria + " Pontos")
